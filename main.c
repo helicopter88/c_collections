4477d6d19c63f691cda1193c9ae15fc0f629845f
Elias Benussi <elias.benussi14@imperial.ac.uk>
2015-12-29 18:54:04 +0100

    corrected application of doubler and insert_heap

--- main.c
+++ main.c
@@ -5,34 +5,32 @@ int comp(int* m, int* n) {
     return 1;
 }
 
-void doubler(int m, int* n) {
+void doubler(int* n) {
     *n = *n * 2;
 }
 
-//int eq(node_t* n1, node_t* n2) {
-//    return (int) n1->data == (int) n2->data;
-//}
+int eq(node_t* n1, node_t* n2) {
+    return (int) n1->data == (int) n2->data;
+}
 
 int main(int argc, char** argv)
 {
     heap_t h = init_heap(comp);
     heap_t* heap = &h;
 
-    insert_heap(heap, 1);
-    insert_heap(heap, 2);
-    insert_heap(heap, 3);
-    insert_heap(heap, 4);
-    insert_heap(heap, 5);
-    insert_heap(heap, 6);
-    insert_heap(heap, 7);
-    insert_heap(heap, 8);
-    insert_heap(heap, 9);
-    insert_heap(heap, 0);
+    for (int i = 0; i < 10; i++) {
+        int* iP = &i;
+        insert_heap(heap, iP);
+    }
 
-    foreach_heap(heap, doubler);
+
+//    printf(heap->nodes[2].data);
+
+    foreach_heap(heap, &doubler);
 
 //    printf(contains_heap(heap, 0, eq));
-//    printf("\n");
+    printf("\n");
+
 //    printf(contains_heap(heap, 15, eq));
 //    printf("\n");
 
