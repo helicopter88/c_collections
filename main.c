4523aa170e7ea26d44e4ebe38df4593d79b131d6
Domenico Fabio Marino <dfm114@ic.ac.uk>
2015-12-31 10:56:32 +0000

    Main: make it compile
    Signed-off-by: Domenico Fabio Marino <dfm114@ic.ac.uk>

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "collections/heap.h"
 
-int comp(int* m, int* n) {
+int comp(T m, T n) {
     return 1;
 }
 
@@ -21,12 +21,12 @@ int eq(T n1, T n2) {
     node_t* node1 = (node_t*) n1;
     node_t* node2 = (node_t*) n2;
 
-    return node1->data == (int) node2->data;
+    return node1->data == node2->data;
 }
 
 int main(int argc, char** argv)
 {
-    heap_t h = init_heap(comp);
+    heap_t h = init_heap(&comp);
     heap_t* heap = &h;
 
     for (int i = 0; i < 10; i++) {
@@ -46,7 +46,7 @@ int main(int argc, char** argv)
 
     foreach_heap(heap, &doubler);
 
-    printf(contains_heap(heap, 0, &eq));
+    printf("%d", contains_heap(heap, 0, &eq));
     printf("\n");
 
 //    printf(contains_heap(heap, 15, eq));
